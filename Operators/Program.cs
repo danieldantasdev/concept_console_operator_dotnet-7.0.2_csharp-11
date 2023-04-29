f91b04fd52c81956d6acf54e7c8ea0c6f6687b01
lobinhodev <contatolobinhodev@gmail.com>
2023-04-28 21:23:41 -0300

    init: commit initial

---
+++ Operators/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// unary ++, --, +, -
+
+int numOperator = 4;
+Console.WriteLine(numOperator++);
+Console.WriteLine(numOperator--);
+
+Console.WriteLine(++numOperator);
+Console.WriteLine(--numOperator);
+
+Console.WriteLine(numOperator);
+Console.WriteLine(-numOperator);
+Console.WriteLine(-(numOperator));
+
+// binary *, /, +, -
+
+var sum = 2 + 2;
+var subtraction = 7 - 2;
+var division = 25 / 2;
+var divisionReal = 2 % 2;
+var multiplication= 2 * 2;
+
+// operator arithmetic <, >, <=, >=
+Console.WriteLine(4 > 5);
+Console.WriteLine(5 >= 5);
+Console.WriteLine(4 < 5);
+Console.WriteLine(4 <= 5);
+
+// operator equalities ==, !=
+
+Console.WriteLine(4 != 5);
+Console.WriteLine(4 == 5);
+
+// operator logic AND &&, OR ||
+
+Console.WriteLine(true || false);
+Console.WriteLine(false || true);
+Console.WriteLine(true || true);
+Console.WriteLine(false || false);
+
+Console.WriteLine(true && false);
+Console.WriteLine(false && true);
+Console.WriteLine(true && true);
+Console.WriteLine(false && false);
